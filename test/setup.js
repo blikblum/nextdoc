d8209d404f1788231ba7e19924d3c285af933857
Luiz Américo Pereira Câmara <brandkarl@gmail.com>
2019-11-02 16:27:49 -0300

    Update dependencies

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,16 @@
 process.stdout.write('Starting test environment');
 
-global.getFile = function(filepath) {
+const getFile = function(filepath) {
   var path = require('path');
   var fs = require('fs');
   return fs.readFileSync(path.resolve(__dirname, '..', filepath)).toString();
 };
+
+const addScript = function(window, script) {
+  const scriptEl = window.document.createElement("script");
+  scriptEl.textContent = script;
+  window.document.body.appendChild(scriptEl);
+}
 
 global.chai = require('chai');
 global.expect = chai.expect;
@@ -12,21 +18,17 @@ global.assert = chai.assert;
 chai.should();
 process.stdout.write('.');
 
-var jsdom = require('jsdom');
+const { JSDOM } = require('jsdom');
 process.stdout.write('. OK\n');
 
 module.exports = function(done) {
-  jsdom.env({
-    html: getFile('test/fixtures/context.html'),
-    src: [
-      getFile('support/vendor/jquery.js'),
-      getFile('nextdoc.js')
-    ],
-    done: function(errors, window) {
-      global.window = window;
-      global.$ = window.jQuery;
-      global.Nextdoc = window.Nextdoc;
-      done(errors);
-    }
-  });
+  JSDOM.fromFile('test/fixtures/context.html', { runScripts: 'dangerously' }).then(dom => {
+    const window = dom.window;
+    addScript(window, getFile('support/vendor/jquery.js'));
+    addScript(window, getFile('nextdoc.js'));
+    global.window = window;
+    global.$ = window.jQuery;
+    global.Nextdoc = window.Nextdoc;
+    done();
+  }).catch(done);
 };
